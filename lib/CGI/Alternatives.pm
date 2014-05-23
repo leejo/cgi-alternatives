b0baf5a1a2a2e9684c86758b4aee63ec928c3233
Lee Johnson <lee@givengain.ch>
2014-05-23 19:50:28 +0200

    skeleton for this dist
    flesh out over the next week or two

---
+++ lib/CGI/Alternatives.pm
@@ -0,0 +1,27 @@
+package CGI::Alternatives;
+
+our $VERSION = '0.03';
+
+=head1 NAME
+
+CGI::Alternatives - documentation for alternative solutions to CGI.pm
+
+=head1 VERSION
+
+0.01
+
+=head1 DESCRIPTION
+
+=head1 PSGI/Plack
+
+=head1 Mojolicious
+
+=head1 Dancer
+
+=head1 Catalyst
+
+=cut
+
+1;
+
+# vim: ts=2:sw=2:et
